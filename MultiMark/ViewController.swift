447e472eb560a0645cb54e27df6bf96de8d24486
Michele Galvagno <michicello@gmail.com>
2019-04-09 14:45:06 +0200

    Manage disconnection

--- MultiMark/ViewController.swift
+++ MultiMark/ViewController.swift
@@ -31,6 +31,20 @@ class ViewController: UIViewController, UITextViewDelegate {
             newWindow.rootViewController = vc
             newWindow.isHidden = false
             self.additionalWindows.append(newWindow)
+            
+            self.textViewDidChange(self.textView)
+        }
+        
+        NotificationCenter.default.addObserver(forName: UIScreen.didDisconnectNotification, object: nil, queue: nil) { [weak self] notification in
+            guard let self = self else { return }
+            
+            guard let oldScreen = notification.object as? UIScreen else { return }
+            
+            if let window = self.additionalWindows.firstIndex(where: {
+                $0.screen == oldScreen
+            }) {
+                self.additionalWindows.remove(at: window)
+            }
         }
     }
     
